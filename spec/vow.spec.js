7e6ee4c917fef7f49bfb4be861b416f13a07cd39
Ægir Örn Símonarson <agirorn@gmail.com>
2017-07-02 15:19:35 +0000

    Fix pending spec.
    Enables user to have pending tests with xit and xdescribe without having
    the test suit fail on this bug.
    
    Fixes #1

--- spec/vow.spec.js
+++ spec/vow.spec.js
@@ -1,51 +1,62 @@
-// const vow = require('../');
+const vow = require('../');
 
-// const orginal = {
-//   it: jasmine.getEnv().it,
-//   beforeEach: jasmine.getEnv().beforeEach,
-//   afterEach: jasmine.getEnv().afterEach,
-//   beforeAll: jasmine.getEnv().beforeAll,
-//   afterAll: jasmine.getEnv().afterAll,
-// };
+const orginal = {
+  it: jasmine.getEnv().it,
+  beforeEach: jasmine.getEnv().beforeEach,
+  afterEach: jasmine.getEnv().afterEach,
+  beforeAll: jasmine.getEnv().beforeAll,
+  afterAll: jasmine.getEnv().afterAll,
+};
 
-// vow(jasmine);
+vow(jasmine);
 
-// describe('vow', () => {
-//   const defer = time => new Promise(resolve => setTimeout(resolve, time));
-//   const delay = (time, cb) => { setTimeout(cb, time); };
+describe('vow', () => {
+  const defer = time => new Promise(resolve => setTimeout(resolve, time));
+  const delay = (time, cb) => { setTimeout(cb, time); };
 
-//   describe('it returning a promise', () => {
-//     let resolved;
-//     beforeEach(() => { resolved = false; });
+  describe('it returning a promise', () => {
+    let resolved;
+    beforeEach(() => { resolved = false; });
 
-//     it('accepts returning a promise', () =>
-//       defer(0).then(() => { resolved = true; }));
+    it('accepts returning a promise', () =>
+      defer(0).then(() => { resolved = true; }));
 
-//     afterEach(() => expect(resolved).toBeTruthy());
-//   });
+    afterEach(() => expect(resolved).toBeTruthy());
+  });
 
-//   describe('it calling done', () => {
-//     let DONE;
-//     beforeEach(() => { DONE = jasmine.createSpy('done'); });
+  describe('it calling done', () => {
+    let DONE;
+    beforeEach(() => { DONE = jasmine.createSpy('done'); });
 
-//     it('ends test when done is called', (done) => {
-//       DONE.and.callFake(done);
-//       delay(0, DONE);
-//     });
+    it('ends test when done is called', (done) => {
+      DONE.and.callFake(done);
+      delay(0, DONE);
+    });
 
-//     afterEach(() => expect(DONE).toHaveBeenCalled());
-//   });
+    afterEach(() => expect(DONE).toHaveBeenCalled());
+  });
 
-//   [
-//     'it',
-//     'beforeEach',
-//     'afterEach',
-//     'beforeAll',
-//     'afterAll',
-//   ].forEach((fn) => {
-//     it(`wraps ${fn}`, () => {
-//       expect(jasmine.getEnv()[fn]).not.toEqual(orginal[fn]);
-//     });
-//   });
-// });
+  [
+    'it',
+    'beforeEach',
+    'afterEach',
+    'beforeAll',
+    'afterAll',
+  ].forEach((fn) => {
+    it(`wraps ${fn}`, () => {
+      expect(jasmine.getEnv()[fn]).not.toEqual(orginal[fn]);
+    });
+  });
+
+  // pending spec should work
+  it('should be pending');
+  xit('should be pending');
+  describe('is not pending', () => {
+    xit('should be pending');
+  });
+
+  xdescribe('should be pending', () => {
+    it('should be pending');
+  });
+});
 
